7972eac5c42d073bc6359f596a3ed2706f0b77cf
Nanxing Chen <chennanxing_hit@163.com>
2026-09-06 11:18:27 +0800

    Add package init

---
+++ src/pfm_mnist/__init__.py
@@ -0,0 +1,5 @@
+"""Photonic Flow Matching for diverse MNIST generation."""
+
+from pfm_mnist.models import ConditionalDiscriminator, PFMGenerator
+
+__all__ = ["PFMGenerator", "ConditionalDiscriminator"]
